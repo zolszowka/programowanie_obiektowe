9885cbde828a1c49b8bdcac6cc74366aacc86aee
zolszowka <zuzanna@olszowka.net>
2022-10-24 20:48:15 +0200

    lab2

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -31,6 +31,15 @@ public class World {
     }
 
     public static void main(String[] args) {
+        //lab2
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
+
+
+        //lab1
         System.out.println("Start");
         Direction[] directions = convert(args);
         run(directions);
